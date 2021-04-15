767415eb7036a02d66991f9790ada38c1f001503
Camila Modena <itscamilla@hotmail.com>
2021-04-15 20:33:50 +0200

    Created and added event listener to mobile menu

--- app/javascript/components/initNavbar.js
+++ app/javascript/components/initNavbar.js
@@ -2,8 +2,20 @@ const initNavbar = () => {
   const mainMenuButton = document.querySelector('.menu-button');
   const mainMenu = document.querySelector('.main-menu');
 
+  const mobileMenuButton = document.querySelector('.navbar-mobile');
+  const mobileMenu = document.querySelector('.main-menu-mobile');
+
   mainMenuButton.addEventListener('click', () => {
     mainMenu.classList.toggle('active');
+  })
+
+  mobileMenuButton.addEventListener('click', () => {
+    mobileMenu.classList.toggle('active');
+    if (mobileMenu.classList.contains('active')) {
+    document.getElementById("menu-text").innerHTML = "TAP  TO  COLLAPSE";
+    } else {
+      document.getElementById("menu-text").innerHTML = "MENU";
+    }
   })
 }
 
